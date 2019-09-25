76ba1a109366571309f6d297aec05e06751a5d1f
ChristianGigante <christian.gigante@student.passerellesnumeriques.org>
2019-09-25 14:51:33 +0800

    updated

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 var app = require('express')();
-var http = require('http').createServer(app);
-var port = process.env.PORT||5000
+var express = require('express');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 5000;
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(port, function(){
-  console.log('listening on *:'+port);
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+	socket.on('username', function (client) {
+		console.log('new user connected');
+		socket.broadcast.emit('username', client);
+	})
+
+
+
+	socket.on('chat message', function (msg) {
+		socket.broadcast.emit('chat message', msg);
+	});
+
+
+
+
+
+	socket.on('typing', function (msg) {
+		socket.broadcast.emit('typing', msg);
+	})
+
+
+
+
+	socket.on('connected', function (msg) {
+		console.log(msg);
+		socket.broadcast.emit('connected', msg);
+	})
+})
+
+
+
+http.listen(port, function () {
+	console.log('listening on *:' + port);
 });

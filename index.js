851697f1979044db045428d988fdd5a2605cce63
ChristianGigante <christian.gigante@student.passerellesnumeriques.org>
2019-09-25 14:07:07 +0800

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var port = process.env.PORT||5000
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(port, function(){
+  console.log('listening on *:'+port);
+});
